41a17ecfaa721163975e939dc2690128217166cf
romulocraveiro <romulocraveiro@gmail.com>
2021-09-30 18:38:47 -0300

    aula 21 explicação

---
+++ aula21-while.py
@@ -0,0 +1,6 @@
+print("Digite um número: ")
+num = int(input())
+while num <=10:
+    
+    print("Digite um número:")#Temos que digitar um comando de saída como abaixo! Se não houver uma condição ou teste de saída, o programa ficará rodando num loop infinito
+    num = int(input()) # Desta vez, o usuário terá o poder de decisão de sair dele ao digitar um número maior que 10
